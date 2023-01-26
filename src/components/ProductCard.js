a1bec4940b320226756451b23e5b3bc358b498bf
mdasif8393 <md.asif.8393@gmail.com>
2023-01-26 22:37:06 +0600

    add folder structure add to cart remove to cart

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,7 +1,14 @@
 import React from "react";
 import { BiListPlus } from "react-icons/bi";
+import { useDispatch } from "react-redux";
+import { ADD_TO_CART } from "../redux/actionTypes/actionTypes";
+import { addToCart, removeFromCart } from "../redux/actionCreators/productActions";
+import {RiDeleteBin2Line} from "react-icons/ri"
+import { useLocation } from "react-router-dom";
 
 const ProductCard = ({ product }) => {
+  const dispatch = useDispatch();
+  const {pathname} = useLocation();
   return (
     <div
       className='shadow-lg rounded-3xl border  p-3 flex flex-col text-indigo-900'
@@ -15,20 +22,32 @@ const ProductCard = ({ product }) => {
       <div className=' flex-1'>
         <ul className='space-y-2'>
           {product.keyFeature.map((feature) => {
-            return <li className='text-sm '>{feature}</li>;
+            return <li className='text-sm ' key={feature}>{feature}</li>;
           })}
         </ul>
       </div>
       <div className='flex gap-2 mt-5'>
-        <button className='bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold'>
+      {pathname.includes("cart") && (
+        <button className='bg-red-500 flex justify-between items-center px-2 rounded-full py-1 flex-1 text-white text-bold'
+        onClick={()=>dispatch(removeFromCart(product))}
+        >
+          <p>Remove</p>
+          <RiDeleteBin2Line size={20}></RiDeleteBin2Line>
+        </button>)}
+
+        {!pathname.includes("cart")&& <button className='bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold'
+        onClick={()=>dispatch(addToCart(product))}
+        >
           Add to cart
-        </button>
-        <button
+        </button>}
+        
+        { !pathname.includes("cart")&&
+          <button
           title='Add to wishlist'
           className='bg-indigo-500  py-1 px-2 rounded-full'
         >
           <BiListPlus className='text-white' />
-        </button>
+        </button>}
       </div>
     </div>
   );
