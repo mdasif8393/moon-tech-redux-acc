48278385ecda9414a66788789cc0bc5b3df3b74f
mdasif8393 <md.asif.8393@gmail.com>
2023-01-27 19:42:44 +0600

    add add-to-cart and remove-from-cart

--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -5,13 +5,33 @@ const initialState = {
 };
 
 const productReducer = (state = initialState, action) => {
+    const selectedProduct = state.cart.find(product => product._id === action.payload._id);
+
     switch(action.type){
         case ADD_TO_CART:
-            return{
+            if(selectedProduct){ 
+                selectedProduct.quantity = selectedProduct.quantity + 1;
+                const newCart = state.cart.filter(product => product._id !== selectedProduct._id);
+
+                return{
+                    ...state,
+                    cart: [...newCart, selectedProduct]
+                }
+            }
+            return{// if not selected product
                 ...state,
-                cart: [...state.cart, action.payload],
+                cart: [...state.cart, {...action.payload, quantity: 1}],
             };
         case REMOVE_FROM_CART: 
+        if(selectedProduct.quantity > 1){ //selected product is undefined here
+            selectedProduct.quantity = selectedProduct.quantity - 1;
+            const newCart = state.cart.filter(product => product._id !== selectedProduct._id);
+
+            return{
+                ...state,
+                cart: [...newCart, selectedProduct]
+            }
+        }
             return{
                 ...state,
                 cart: state.cart.filter((product) => product._id !== action.payload._id),
